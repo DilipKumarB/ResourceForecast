23c376263ce3b9bf9d8273200022b5cda15ad4f2
DilipKumarB <baskaran.dilipkumar@gmail.com>
2021-08-17 00:27:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -51,4 +51,4 @@ def predict():
     return render_template('index.html', prediction_text='Resource Cost = {}'.format(output))
 
     if __name__=="__main__":
-    app.run(debug=True)
+        app.run(debug=True)
